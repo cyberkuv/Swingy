eceef1df6bfcc4aab9d963597d75d946582421a0
emmanuel ONWE <eonwe@c2r4s1.wethinkcode.co.za>
2019-08-07 14:25:20 +0200

    simple swing gui added

--- src/main/java/za/wethinkcode/swingy/GUI/Gui.java
+++ src/main/java/za/wethinkcode/swingy/GUI/Gui.java
@@ -1,16 +1,48 @@
 package za.wethinkcode.swingy.GUI;
 
-import java.awt.Button;
-import java.awt.Frame;
+import java.awt.Color;
+import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Gui extends Frame {
+import javax.swing.*;
+
+public class Gui {
+    JFrame f = new JFrame("_SWINGY SWINGY_");
     Gui() {
-        Button newHero = new Button("New Hero");
-        newHero.setBounds(30, 100, 80, 30);
-        add(newHero);
-        setSize(600, 800);
-        setLayout(null);
-        setVisible(true);
+        JButton b1 = new JButton("New Hero");
+        JButton b2 = new JButton("Prev Hero");
+        f.setBackground(Color.BLACK);
+        f.getContentPane().setBackground(Color.BLACK);
+
+        b1.setBounds(250, 250, 150, 40);
+        b1.setBackground(Color.GRAY);
+        b1.setBorderPainted(false);
+        b1.setContentAreaFilled(false);
+        b1.setOpaque(true);
+        b1.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                NewHero();
+            }
+        });
+
+        b2.setBounds(250, 450, 150, 40);
+        b2.setBackground(Color.GRAY);
+        b2.setBorderPainted(false);
+        b2.setContentAreaFilled(false);
+        b2.setOpaque(true);
+
+        f.add(b1);
+        f.add(b2);
+        f.setSize(700, 800);
+        f.setLayout(null);
+        f.setVisible(true);
     }
-    public static void main(String[] args) { Gui gui = new Gui(); }
+    public void NewHero() {
+        Panel panel = new Panel();
+        panel.setBackground(Color.GRAY);
+        f.add(panel);
+    }
+    public static void main(String[] args) { new Gui(); }
 }
